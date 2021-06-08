122df579cc113b66bac580f43d5ea9184c8c2ef1
AD9018 <adamdadi23@gmail.com>
2021-06-08 15:58:07 -0400

    WIP

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
 const express = require("express")
 
+const CarRouter = require("./cars/cars-router")
+
+
 const server = express()
+server.use('/api/cars',CarRouter)
 
 // DO YOUR MAGIC
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
 
 module.exports = server
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,17 @@
 // DO YOUR MAGIC
+exports.up = function(knex){
+    return knex.schema.createTable('cars', tbl => {
+     tbl.increments()
+     tbl.string('vin').notNullable().unique()
+     tbl.string('make').notNullable()
+     tbl.string('model').notNullable()
+     tbl.integer('mileage').notNullable()
+     tbl.string('optional')
+     tbl.string('transmission')
+    })
+}
+
+
+exports.down = function(knex){
+    return knex.schema.dropTableIfExists('cars')
+}
